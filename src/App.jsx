ebeb9f2a6c53767bd90acfa1a24aa2f69a1a2476
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-12-23 12:56:37 -0500

    Fix router basename

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
