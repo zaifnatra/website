c50d1dbebceda7a806b029010d6b7eb137cf4ef3
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-12-30 00:07:06 -0500

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import heroes2 from '../assets/heroes2.jpg';
 import campus from '../assets/campus.jpg';
 import ev from '../assets/ev.png';
 import s1 from '../assets/S1.jpg';
-import s2 from '../assets/S2jpg';
+import s2 from '../assets/S2.jpg';
 const Project = ({ title, desc, stack, images, githubLink, downloadLink, liveLink }) => (
     <div style={{
         padding: '2rem',
